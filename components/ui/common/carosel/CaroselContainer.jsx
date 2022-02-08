37ad7a02e38f16288bfa185bd742cc2afde00259
Alwaz18 <alwazk8@gmail.com>
2022-02-09 02:16:39 +0500

    ..

--- components/ui/common/carosel/CaroselContainer.jsx
+++ components/ui/common/carosel/CaroselContainer.jsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-
-import { Container } from "@chakra-ui/react";
-
 import Carosel from "./HeroCarosel";
 import Image from "next/image";
 
@@ -14,38 +11,17 @@ const CaroselContainer = () => {
   ];
 
   return (
-    <Container
-      // py={8}
-      // px={0}
-      minW="100%"
-      // p={2}
-      // maxW="60%"
-      mx="auto"
-    >
+    <div className="-my-2">
       <Carosel gap={10}>
         {data?.map(({ title, src }, index) => (
           <div className="relative min-h-full min-w-full" key={index}>
             <div className="min-h-[500px] min-w-full">
-              <Image
-                src={src}
-                alt="me"
-                // width="300px"
-                // height="100px"
-                layout="fill"
-                // objectFit="contain"
-                // width="10000px"
-                // height="500px"
-                // layout="fixed"
-                objectFit="cover"
-              />
+              <Image src={src} alt="me" layout="fill" objectFit="cover" />
             </div>
-            {/* <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
-                {title}
-              </Heading> */}
           </div>
         ))}
       </Carosel>
-    </Container>
+    </div>
   );
 };
 
